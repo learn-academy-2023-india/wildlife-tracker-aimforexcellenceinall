748e381423e0fd63754b650ea9f53889330f2908
user.email <learnacademy@LEARNs-Air.attlocal.net>
2024-02-01 13:13:27 -0800

    adds animal CRUD actions

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,49 @@
+class AnimalsController < ApplicationController
+
+    # GET /animals
+    def index
+        @animals = Animal.all
+        render json: @animals
+    end
+
+    #GET /animals/1
+    def show
+        render json: @animal
+    end
+
+    # POST /animals
+    def create
+        @animal = Animal.new(animal_params)
+        if @animal.save
+            render json: @animal, status: :created, location: @animal
+        else
+            render json: @animal.errors, status: :unprocessable_entity
+        end
+    end
+
+    # PUT/PATCH /animnals/1
+    def update
+        if @animal.update(animal_params)
+            render json: @animal
+        else 
+            render json: @animal.errors, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE /animals/1
+    def destroy
+        @animal.destroy
+    end
+
+    # GET /animals/1/animal_type
+    private
+    # Returns the animal with the given id
+    def set_animal
+        @animal = Animal.find(params[:id])
+    end
+
+    # Allows certain data to be passed
+    def animal_params
+        params.require(:animal).permit(:common_name, :scientific_binomial)
+    end
+end
